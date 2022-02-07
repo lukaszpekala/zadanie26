2d93fdecaa84fea05123d045622f63da2de7d302
lukaszpekala <lukasz.pekala.pl@gmail.com>
2022-02-07 14:47:37 +0100

    init

---
+++ src/test/java/com/example/betandwin/BetAndWinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.betandwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetAndWinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
